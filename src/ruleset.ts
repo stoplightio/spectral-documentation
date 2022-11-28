252166b6916af9c72fe93f4cc6feef7f63e45b3f
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2022-11-28 16:36:15 +0000

    feat: docs-info-contact, docs-tags-alphabetical and others

--- src/ruleset.ts
+++ src/ruleset.ts
@@ -1,74 +1,64 @@
-import { pattern, schema, truthy, length } from "@stoplight/spectral-functions";
+import { alphabetical, pattern, schema, truthy, length } from "@stoplight/spectral-functions";
 import { oas2, oas3 } from "@stoplight/spectral-formats";
 import { DiagnosticSeverity } from "@stoplight/types";
 
 
 export default {
+
+  // extends: [
+  //   'spectral:oas'
+  // ],
+
   aliases: {
-    SchemaObjects: {
-      description: "Schemas, and/or sub-schemas should define an inline example, default, enum, or some sort of static value.",
+    // OAS Aliases
+    // TODO Remove these once they're available in spectral:oas
+    PathItem: ['$.paths[*]'],
+    OperationObject: ['#PathItem[get,put,post,delete,options,head,patch,trace]'],
+
+    // Custom Aliases
+    SharedSchemaObjects: {
+      description: "Shared schemas (a.k.a Models) are the main domain models and need to be well documented.",
       targets: [
         {
           formats: [oas2],
-          given: [
-            '$.paths[*][*]..content[*]',
-            '$.paths[*][*]..headers[*]',
-            '$.paths[*][*]..parameters[*]',
-          ]
+          given: ['$.definitions[*]'],
         },
         {
           formats: [oas3],
-          given: [
-            '$.paths[*][*]..headers[*].schema',
-            '$.paths[*][*]..parameters[*].schema',
-            
-            // TODO is this redundant because of $ref?
-            '$.components.headers[*].schema',
-            '$.components.parameters[*].schema',
-
-            // TODO Ok for now but x-examples with a 'type' show up in it
-            "$.components.schemas[*]..[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
-
-            // All schemas should have an example defined inline, unless its got items or properties in which case _they_ should.
-            // This relies on type, but tbh I could not find a less greedy JSON Path.
-            "$.paths[*][*]..content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
-            "$..responses[*].content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
-          ]
+          given: ['$.components.schemas[*]'],
         }
       ]
     },
 
-    OasObjectsThatShouldHaveExampleOrExamples: {
-      description: "Not Schemes, but these OpenAPI objects should define some sort of example or examples directly.",
+    DescribableObjects: {
+      description: "All objects that should be described.",
       targets: [
         {
           formats: [oas2],
           given: [
-            '$.paths[*][*]..content[*]',
-            '$.paths[*][*]..headers[*]',
-            '$.paths[*][*]..parameters[*]',
-          ]
+            '$.info',
+            '$.tags[*]',
+            '#OperationObject',
+            '$.paths[*][*].responses[*]',
+            '$..parameters[?(@ && @.in)]',
+          ],
         },
         {
           formats: [oas3],
           given: [
-            '$.paths[*][*]..headers[*]',
-            '$.paths[*][*]..parameters[*]',
-            
-            '$.components.headers[*]',
-            '$.components.parameters[*]',
-
-            // TODO Ok for now but x-examples with a 'type' show up in it
-            "$.components.schemas[*]..[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
-
-            // All schemas should have an example defined inline, unless its got items or properties in which case _they_ should.
-            // This relies on type, but tbh I could not find a less greedy JSON Path.
-            "$.paths[*][*]..content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
-            "$..responses[*].content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
-          ]
+            '$.info',
+            '$.tags[*]',
+            '#OperationObject',
+            '$.paths[*][*].responses[*]',
+            '$..parameters[?(@ && @.in)]',
+            '$.definitions[*]',
+            '$.components.schemas[*]',
+            '$.servers[*]',
+          ],
         }
       ]
-    }
+    },
+
   },
 
   rules: {
@@ -76,136 +66,10 @@ export default {
     /**
      * @author: Phil Sturgeon <https://github.com/philsturgeon>
      */
-    'docs-definitions-description': {
-      message: "Definitions SHOULD contain descriptions.",
-      description: "Definition descriptions should be used to explain what this domain object is, in terms useful to an API consumer who may have less context.",
-      severity: DiagnosticSeverity.Warning,
-      given: "$..definitions",
-      formats: [oas2],
-      then: {
-        field: 'description',
-        function: truthy,
-      }
-    },
-
-
-    // - docs-description
-    //     - given:
-    //         - $.info
-    //         - $..parameters[?(@ && @.in)]
-    //         - #PathItem.parameters[?(@ && @.in)]'
-    //         - #OperationObject.parameters[?(@ && @.in)]
-    //         - $.components.parameters[?(@ && @.in)]
-    //     - field: description
-    //     - truthy
-        
-    // - docs-description-length
-    //     - given: ditto 👆
-    //     - field: description
-    //     - minLength: 20
-        
-    // - docs-description-end-stop - Always end with a full stop
-    //     - given: ditto 👆
-    //     - field: description
-    //     - endWith: `.`
-        
-    // - docs-description-capitalize
-    //     - given: ditto 👆
-    //     - field: description
-    //     - pattern: /^[A-Z]/
-
-    'docs-parameter-example': {
-      message: "Parameters SHOULD contain examples.",
-      description: "Documentation tools use examples to provide more realistic representations of wht is contained in the parameter, so users can send valid requests sooner.",
-      severity: DiagnosticSeverity.Warning,
-      given: "$..parameters[*].schema",
-      formats: [oas3],
-      then: {
-        function: schema,
-        functionOptions: {
-          schema: {
-            oneOf: [
-              { required: ['examples'] },
-              { required: ['example'] },
-              { required: ['default'] },
-              { required: ['enum'] },
-              { required: ['const'] },
-            ]
-          }
-        }
-      }
-    },
-
-    'docs-response-example': {
-      message: "Responses SHOULD contain examples.",
-      description: "Documentation tools use examples to provide more realistic representations of the request/response bodies.",
-      severity: DiagnosticSeverity.Warning,
-      given: "$..responses[*].content[*]",
-      formats: [oas3],
-      then: {
-        function: schema,
-        functionOptions: {
-          schema: {
-            oneOf: [
-              { required: ['examples'] },
-              { required: ['example'] },
-            ]
-          }
-        }
-      }
-    },
-    
-    'docs-examples-or-defaults':{
-      message: "Responses SHOULD contain some sort of examples or default value.",
-      description: "Documentation tools use examples or any other form of default value, enum, or const defined to pick a value to show in an example.",
-      severity: DiagnosticSeverity.Warning,
-      given: "#ObjectsThatShouldHaveExamples",
-      then: {
-        function: schema,
-        functionOptions: {
-          schema: {
-            oneOf: [
-              { required: ['examples'] },
-              { required: ['example'] },
-              { required: ['x-examples'] },
-              { required: ['x-example'] },
-              { required: ['default'] },
-              { required: ['enum'] },
-              { required: ['const'] },
-            ]
-          }
-        }
-      }
-    },
-        
-    //     - field: examples
-    //     - function: truthy
-    // - docs-media-examples-oas3
-    //     - given
-            
-    //         ```jsx
-    //         '$..content..[?(@ && @.schema)]',
-    //         '$..headers..[?(@ && @.schema)]',
-    //         '$..parameters..[?(@ && @.schema)]',
-    //         ```
-            
-    //     - function: schema
-    //     - oneOf: example or examples
-    // - docs-media-examples-oas2 (maybe dont bother with OAS2?)
-    //     - given: `$..responses..[?(@ && @.schema)]`
-    //     - truthy: `examples`
-
-    // ### Enforce descriptions and length
-
-    /**
-     * @author: Phil Sturgeon <https://github.com/philsturgeon>
-     */
-    'docs-info-description': {
-      message: "Descriptions are important, don't scrimp on them.",
+    'docs-descriptions': {
       description: "Documentation tools use description to provide more context to users of the API who are not as familiar with the concepts as the API designers are.",
       severity: DiagnosticSeverity.Warning,
-      given: "$.info",
-      formats: [oas2],
+      given: "#DescribableObjects",
       then: [
         {
           field: 'description',
@@ -234,16 +98,77 @@ export default {
         },
       ]
     },
+    
+    'docs-info-contact': {
+      description: 'Providing contact means that API consumers can get in touch with you, which can be confusing even if you all work at the same company. This could be a specific developer or a team, depending on the organization.',
+      severity: DiagnosticSeverity.Warning,
+      given: '$',
+      then: {
+        field: 'info.contact',
+        function: truthy,
+      },
+    },
 
+    'docs-tags-alphabetical': {
+      message: "Tags should be defined in alphabetical order",
+      description: 'Many documentation tools show tags in the order they are defined, so defining them not in alphabetical order can look funny to API consumers.',
+      severity: DiagnosticSeverity.Warning,
+      given: '$',
+      then: {
+        field: 'tags',
+        function: alphabetical,
+        functionOptions: {
+          keyedBy: 'name',
+        },
+      },
+    },
+
+    'docs-operationId-valid-in-url': {
+      message: 'operationId must not characters that are invalid when used in URL.',
+      description: 'Most documentation tools use the operationId to produce URLs, so the characters used must be URL friendly.',
+      given: '#OperationObject',
+      severity: DiagnosticSeverity.Error,
+      then: {
+        field: 'operationId',
+        function: pattern,
+        functionOptions: {
+          match: "^[A-Za-z0-9-._~:/?#\\[\\]@!\\$&'()*+,;=]*$",
+        },
+      },
+    },
+
+    'docs-operation-tags': {
+      description: 'Operation must have non-empty `tags` array.',
+      severity: DiagnosticSeverity.Warning,
+      given: '#OperationObject',
+      then: {
+        field: 'tags',
+        function: schema,
+        functionOptions: {
+          schema: {
+            type: 'array',
+            minItems: 1,
+          },
+        },
+      },
+    },
+
+    /**
+     * @author: Phil Sturgeon <https://github.com/philsturgeon>
+     */
+    'docs-schema-description': {
+      message: "Schemas should contain descriptions.",
+      description: "API consumers have less context than the API developers, so providing high quality descriptions of shared schemas (a.k.a Models or Definitions) can help a lot. Explain what this schema is, in terms useful to an API consumer who might be coming across these terms for the first time, or might assume a different meaning to the one intended.",
+      severity: DiagnosticSeverity.Warning,
+      given: "#SharedSchemaObjects",
+      then: {
+        field: 'description',
+        function: truthy,
+      }
+    },
 
     // ### Grammar check
 
-    // We could shove this in there? 
-
-    // [https://github.com/api-stuff/spectral-spelling-grammar](https://github.com/api-stuff/spectral-spelling-grammar)
-
-    // ### Other ideas
-
-    // -
+    // https://github.com/api-stuff/spectral-spelling-grammar
   },
 };
