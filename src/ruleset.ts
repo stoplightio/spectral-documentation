fce90663543c0887c3f86873cc285877393ff2ee
Phil Sturgeon <67381+philsturgeon@users.noreply.github.com>
2022-11-28 13:33:42 +0000

    stashing this code soup as im about to axe it all

--- src/ruleset.ts
+++ src/ruleset.ts
@@ -2,70 +2,134 @@ import { pattern, schema, truthy, length } from "@stoplight/spectral-functions";
 import { oas2, oas3 } from "@stoplight/spectral-formats";
 import { DiagnosticSeverity } from "@stoplight/types";
 
+
 export default {
   aliases: {
-    'ObjectsThatShouldHaveExamples': [
-      '$.components.headers[*]',
-      '$.components.parameters[*]',
-      '$.paths[*][*]..content[*]',
-      '$.paths[*][*]..headers[*]',
-      '$.paths[*][*]..parameters[*]',
-    ],
+    SchemaObjects: {
+      description: "Schemas, and/or sub-schemas should define an inline example, default, enum, or some sort of static value.",
+      targets: [
+        {
+          formats: [oas2],
+          given: [
+            '$.paths[*][*]..content[*]',
+            '$.paths[*][*]..headers[*]',
+            '$.paths[*][*]..parameters[*]',
+          ]
+        },
+        {
+          formats: [oas3],
+          given: [
+            '$.paths[*][*]..headers[*].schema',
+            '$.paths[*][*]..parameters[*].schema',
+            
+            // TODO is this redundant because of $ref?
+            '$.components.headers[*].schema',
+            '$.components.parameters[*].schema',
 
-    // SharedParameterObject: [
-    //   description: an optional property describing the purpose of the alias
-    //   targets:
-    //     - formats:
-    //         - oas2
-    //       given:
-    //         - $.parameters[*]
-    //     - formats:
-    //         - oas3
-    //       given:
-    //         - $.components.parameters[*]
+            // TODO Ok for now but x-examples with a 'type' show up in it
+            "$.components.schemas[*]..[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
+
+            // All schemas should have an example defined inline, unless its got items or properties in which case _they_ should.
+            // This relies on type, but tbh I could not find a less greedy JSON Path.
+            "$.paths[*][*]..content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
+            "$..responses[*].content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
+          ]
+        }
+      ]
+    },
+
+    OasObjectsThatShouldHaveExampleOrExamples: {
+      description: "Not Schemes, but these OpenAPI objects should define some sort of example or examples directly.",
+      targets: [
+        {
+          formats: [oas2],
+          given: [
+            '$.paths[*][*]..content[*]',
+            '$.paths[*][*]..headers[*]',
+            '$.paths[*][*]..parameters[*]',
+          ]
+        },
+        {
+          formats: [oas3],
+          given: [
+            '$.paths[*][*]..headers[*]',
+            '$.paths[*][*]..parameters[*]',
+            
+            '$.components.headers[*]',
+            '$.components.parameters[*]',
+
+            // TODO Ok for now but x-examples with a 'type' show up in it
+            "$.components.schemas[*]..[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
+
+            // All schemas should have an example defined inline, unless its got items or properties in which case _they_ should.
+            // This relies on type, but tbh I could not find a less greedy JSON Path.
+            "$.paths[*][*]..content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
+            "$..responses[*].content[*].schema..*[?(@property !== 'properties' && @property === 'type' && @ && (@.items === void 0 || @.properties === void 0 ))]^",
+          ]
+        }
+      ]
+    }
   },
 
   rules: {
+
     /**
      * @author: Phil Sturgeon <https://github.com/philsturgeon>
      */
-    'docs-definitions-example': {
-      message: "Definitions SHOULD contain examples.",
-      description: "Documentation tools use examples to provide more realistic representations of the request/response bodies.",
+    'docs-definitions-description': {
+      message: "Definitions SHOULD contain descriptions.",
+      description: "Definition descriptions should be used to explain what this domain object is, in terms useful to an API consumer who may have less context.",
       severity: DiagnosticSeverity.Warning,
-      given: "$..definitions..[?(@property !== 'properties' && @ && (@.items === void 0 || @.properties === void 0 ))]",
+      given: "$..definitions",
       formats: [oas2],
       then: {
-        function: schema,
-        functionOptions: {
-          schema: {
-            oneOf: [
-              'examples',
-              'example',
-              'x-example',
-              'default',
-              'enum',
-            ]
-          }
-        }
+        field: 'description',
+        function: truthy,
       }
     },
 
+
+    // - docs-description
+    //     - given:
+    //         - $.info
+    //         - $..parameters[?(@ && @.in)]
+    //         - #PathItem.parameters[?(@ && @.in)]'
+    //         - #OperationObject.parameters[?(@ && @.in)]
+    //         - $.components.parameters[?(@ && @.in)]
+    //     - field: description
+    //     - truthy
+        
+    // - docs-description-length
+    //     - given: ditto 👆
+    //     - field: description
+    //     - minLength: 20
+        
+    // - docs-description-end-stop - Always end with a full stop
+    //     - given: ditto 👆
+    //     - field: description
+    //     - endWith: `.`
+        
+    // - docs-description-capitalize
+    //     - given: ditto 👆
+    //     - field: description
+    //     - pattern: /^[A-Z]/
+
     'docs-parameter-example': {
       message: "Parameters SHOULD contain examples.",
-      description: "Documentation tools use examples to provide more realistic representations of the request/response bodies.",
+      description: "Documentation tools use examples to provide more realistic representations of wht is contained in the parameter, so users can send valid requests sooner.",
       severity: DiagnosticSeverity.Warning,
-      given: "$..parameters..[?(@property !== 'properties' && @ && (@.items === void 0 || @.properties === void 0 ))",
+      given: "$..parameters[*].schema",
       formats: [oas3],
       then: {
         function: schema,
         functionOptions: {
           schema: {
             oneOf: [
-              'examples',
-              'default',
-              'enum',
-              'const',
+              { required: ['examples'] },
+              { required: ['example'] },
+              { required: ['default'] },
+              { required: ['enum'] },
+              { required: ['const'] },
             ]
           }
         }
@@ -76,25 +140,21 @@ export default {
       message: "Responses SHOULD contain examples.",
       description: "Documentation tools use examples to provide more realistic representations of the request/response bodies.",
       severity: DiagnosticSeverity.Warning,
-      given: "$..responses..[?(@property !== 'properties' && @ && (@.items === void 0 || @.properties === void 0 ))",
+      given: "$..responses[*].content[*]",
       formats: [oas3],
       then: {
         function: schema,
         functionOptions: {
           schema: {
             oneOf: [
-              'examples',
-              'example',
-              'default',
-              'enum',
-              'const',
+              { required: ['examples'] },
+              { required: ['example'] },
             ]
           }
         }
       }
     },
     
-  
     'docs-examples-or-defaults':{
       message: "Responses SHOULD contain some sort of examples or default value.",
       description: "Documentation tools use examples or any other form of default value, enum, or const defined to pick a value to show in an example.",
@@ -105,13 +165,13 @@ export default {
         functionOptions: {
           schema: {
             oneOf: [
-              'examples',
-              'example',
-              'x-example',
-              'x-examples',
-              'default',
-              'enum',
-              'const',
+              { required: ['examples'] },
+              { required: ['example'] },
+              { required: ['x-examples'] },
+              { required: ['x-example'] },
+              { required: ['default'] },
+              { required: ['enum'] },
+              { required: ['const'] },
             ]
           }
         }
@@ -175,30 +235,6 @@ export default {
       ]
     },
 
-    // - docs-description
-    //     - given:
-    //         - $.info
-    //         - $..parameters[?(@ && @.in)]
-    //         - #PathItem.parameters[?(@ && @.in)]'
-    //         - #OperationObject.parameters[?(@ && @.in)]
-    //         - $.components.parameters[?(@ && @.in)]
-    //     - field: description
-    //     - truthy
-        
-    // - docs-description-length
-    //     - given: ditto 👆
-    //     - field: description
-    //     - minLength: 20
-        
-    // - docs-description-end-stop - Always end with a full stop
-    //     - given: ditto 👆
-    //     - field: description
-    //     - endWith: `.`
-        
-    // - docs-description-capitalize
-    //     - given: ditto 👆
-    //     - field: description
-    //     - pattern: /^[A-Z]/
 
     // ### Grammar check
 
