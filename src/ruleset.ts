521aeb1af81cab4c9269f78744b816ea0c4ddc75
Nauman Ali <mnaumanali94@gmail.com>
2024-09-13 23:50:34 +0500

    fix: remove typo

--- src/ruleset.ts
+++ src/ruleset.ts
@@ -200,7 +200,7 @@ export default {
           field: "description",
           function: patternWithoutMd,
           functionOptions: {
-            matsadach: "\\.$",
+            match: "\\.$",
           },
         },
       ],
